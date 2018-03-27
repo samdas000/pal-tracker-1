018f68875aec48bd31efa830cad00e66d64a3c7e
pichaporn-s <pichaporn.s@extend-it-resource.com>
2018-03-27 10:30:01 +0800

    Simple Spring Boot app

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.pal.tracker;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class PalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+}
